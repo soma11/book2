b479b3c1b5effcb3b6c0a7ad6776a765180d533d
Soma Ichikawa <your.email@example.com>
2016-05-14 19:23:27 +0900

    refs task/#63051

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
     resources :receipts
     namespace :m do
       resources :books
+      resources :branches
     end
   end
   resource :dashboard, only: [:show]
