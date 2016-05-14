40feb869767929f4e4fbb2b0ab3a9f9786c2a2c6
角勝平 <neeyonnmaruz@gmail.com>
2016-05-14 17:47:12 +0900

    Productモデル修正

--- db/migrate/20160514064937_create_products.rb
+++ db/migrate/20160514064937_create_products.rb
@@ -3,8 +3,6 @@ class CreateProducts < ActiveRecord::Migration
     create_table :products do |t|
       t.integer :m_book_id
       t.integer :status
-      t.datetime :requested_date
-      t.integer :request_user_id
       t.integer :rental_user_id
 
       t.timestamps null: false
