48f3385ce2605c86c8c3ba1d886da11f86f92f3b
narasaki <mduckfrogbby@gmail.com>
2016-05-15 14:51:02 +0900

    task/63062  プロフィール情報更新画面作成（メアド更新のみ）

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
   resources :purchase_requests
   resources :receipt_images, only: [:show]
   devise_for :users, path_names: { sign_in: "login", sign_out: "logout"},
-    controllers: { omniauth_callbacks: "omniauth_callbacks" }
+    controllers: { 
+      registrations: "users/registrations",
+      omniauth_callbacks: "omniauth_callbacks" }
   resources :users, only: [:show]
   namespace :admin do
     resources :receipts
